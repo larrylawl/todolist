6734201371567eca6f2e9f8334571eff15e65c42
Larry Law <larrylaw97@gmail.com>
2018-12-25 14:21:47 +0800

    submit w enter

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,3 +1,8 @@
 $(document).on("turbolinks:load",function () {
-	$("#todo").focus();
+	$("#task_todo").focus();
+	$("form").keypress(function(event) {
+		if(event.which == 13) {
+			$(this).trigger('submit.rails');
+		}
+	});
 })
