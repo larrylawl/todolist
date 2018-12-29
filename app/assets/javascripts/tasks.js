a4fca7c96efbc3c66f64913e359f85d876ca388b
Larry Law <larrylaw97@gmail.com>
2018-12-29 20:05:57 +0800

    abstracted out reset form

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,12 +1,37 @@
-$(document).on("turbolinks:load",function () {
-	// Subtasks
-	$("section#subtasks").hide();
+/*
+Resets form by reseting value on TODO box, focuses on it, and
+submit upon hitting enter key
+Resets tag if there's a value
+Abstracted out from 4 javascript files!
+*/
+function reset_form(selector) {
+	const todo = `${selector} [name*=todo]`
+	const tag = `${selector} [name*=tag_list]`
+	$(todo).val("");
+	$(todo).focus();
+	submit_enter(todo);
+	if (element_exist(tag)){
+		$(tag).val("");
+		submit_enter(tag);
+	} else {};
+}
 
-	// Task
-	$("#task_todo").focus();
-	$("form").keypress(function(event) {
+function element_exist(selector) {
+	return $(`${selector}`.length > 0)
+}
+
+function submit_enter(selector) {
+	$(selector).keypress(function(event) {
 		if(event.which == 13) {
 			$(this).trigger('submit.rails');
 		}
 	});
+}
+
+$(document).on("turbolinks:load",function () {
+	// Subtasks
+	$("section#subtasks").hide();
+	reset_form("#task_form");
 })
+
+
