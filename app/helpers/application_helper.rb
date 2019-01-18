4f1794de0355804cc2e3ea825a53ee81a89ec2dc
Larry Law <larrylaw97@gmail.com>
2019-01-18 16:43:56 +0800

    doc halfway

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
-	# Object --> String
+	# Returns "checked" if object's completed attribute equals 1,
+	# and "unchecked" otherwise.
 	def get_status(object)
 		if object.completed == 1
 			"checked"
